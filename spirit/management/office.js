415a4704f0e8dcae93d22db802e23deed8d1f215
星川かえで <futursolo@hoshikawakaede-no-MacBook-Pro.local>
2015-06-25 07:58:09 +0800

    weekly commit

--- spirit/management/office.js
+++ spirit/management/office.js
@@ -15,26 +15,59 @@
 
 window.loading = true;
 
-var _ = document.querySelector;
+var _ = function (selector) {
+    return document.querySelector(selector);
+};
 
-var _All = document.querySelectorAll;
+var _All = function (selector) {
+    return document.querySelectorAll(selector);
+};
 
 var objects = {
     ".load": _(".load"),
     ".load .failed": _(".load .failed"),
 
-    ".header .toggle-menu": _(".header .toggle-menu"),
-    ".header .reload-page": _(".header .reload-page"),
+    ".public": _(".public"),
+
+    "header .toggle-sidebar": _("header .toggle-sidebar"),
+    "header .reload-page": _("header .reload-page"),
 
     ".main aside": _(".main aside"),
+    ".main aside .show-lobby": _(".main aside .show-lobby"),
+    ".main aside .show-working": _(".main aside .show-working"),
+    ".main aside .show-crda": _(".main aside .show-crda"),
+    ".main aside .show-configuration": _(".main aside .show-configuration"),
 
     ".main > .lobby": _(".main > .container.lobby"),
-    ".main > .lobby .writing_num .content": _(".main > .container.lobby .tile.writing_num .content"),
-    ".main > .lobby .page_num .content": _(".main > .container.lobby .tile.page_num .content"),
-    ".main > .lobby .reply_num .content": _(".main > .container.lobby .tile.reply_num .content"),
+    ".main > .lobby .writing-num .content": _(".main > .container.lobby .tile.writing-num .content"),
+    ".main > .lobby .page-num .content": _(".main > .container.lobby .tile.page-num .content"),
+    ".main > .lobby .reply-num .content": _(".main > .container.lobby .tile.reply-num .content"),
 
-    ".working .time-input": _(".working > .info-container .time-container > .time-input"),
-    ".working .time-checkbox": _(".working > .info-container .time-container > .time-ckeckbox")
+
+    ".main > .working": _(".main > .working"),
+    ".main > .working .title-input": _(".main > .container.working > .title-container > .title-input"),
+    ".main > .working .editor-textarea": _(".main > .container.working > .editor-container > .editor-textarea"),
+    ".main > .working .preview-div": _(".main > .container.working > .preview-container > .preview-div"),
+
+    ".main > .working .slug-input": _(".main > .container.working  > .info-container .slug-container > .slug-input"),
+    ".main > .working .slug-preview": _(".main > .container.working  > .info-container .slug-container > .slug-preview"),
+
+    ".main > .working .type-radio-group": _(".main > .container.working  > .info-container .type-container > .type-radio-group"),
+
+    ".main > .working .time-checkbox": _(".main > .container.working  > .info-container .time-container > .time-checkbox"),
+    ".main > .working .time-input": _(".main > .container.working > .info-container .time-container > .time-input"),
+
+
+    ".main > .working .info-container": _(".main > .container.working > .info-container"),
+
+    ".main > .working .info-container .close-button": _(".main > .container.working > .info-container .close-button"),
+
+    ".main > .working .id-input": _(".main > .container.working  > .info-container .hidden-container > .id-input"),
+    ".main > .working .method-input": _(".main > .container.working  > .info-container .hidden-container > .method-input"),
+    ".main > .working .publish-or-not": _(".main > .container.working  > .info-container .hidden-container > .publish-or-not"),
+
+    ".main > .working .float-container .publish-fab": _(".main > .container.working  > .float-container > .publish-fab"),
+    ".main > .working .float-container .open-public-fab": _(".main > .container.working  > .float-container > .open-public-fab")
 };
 
 function getCookie(name) {
@@ -110,6 +143,19 @@ function pushState(uri) {
     }
 }
 
+var getVars = (function () {
+    var vars = {};
+    var param = location.search.substring(1).split("&");
+    for (var i = 0; i < param.length; i++) {
+        var keySearch = param[i].search(/=/);
+        var key = "";
+        if (keySearch != -1) key = param[i].slice(0, keySearch);
+        var val = param[i].slice(param[i].indexOf("=", 0) + 1);
+        if(key !== "") vars[key] = decodeURI(val);
+    }
+    return vars;
+})();
+
 function loadLayout(callback) {
     window.loading = true;
 
@@ -144,6 +190,20 @@ window.addEventListener("load", function () {
     );
 });
 
+function hidePublic() {
+    objects[".public"].classList.remove("visible");
+    setTimeout(function () {
+    objects[".public"].style.height = "0";
+    objects[".public"].style.width = "0";
+    }, 300);
+}
+
+function showPublic() {
+    objects[".public"].style.height = "100%";
+    objects[".public"].style.width = "100%";
+    objects[".public"].classList.add("visible");
+}
+
 function loadLobbyData(callback) {
     queryString = new FormData();
     queryString.append("_xsrf", getCookie("_xsrf"));
@@ -161,9 +221,9 @@ function loadLobbyData(callback) {
     }).then(function (resp) {
         var data = resp;
         console.log(data);
-        objects[".main > .lobby .writing_num .content"].innerHTML = data.writings;
-        objects[".main > .lobby .page_num .content"].innerHTML = data.pages;
-        objects[".main > .lobby .reply_num .content"].innerHTML = data.replies;
+        objects[".main > .lobby .writing-num .content"].innerHTML = data.writings;
+        objects[".main > .lobby .page-num .content"].innerHTML = data.pages;
+        objects[".main > .lobby .reply-num .content"].innerHTML = data.replies;
         if (callback) {
             callback();
         }
@@ -184,20 +244,22 @@ function showLobby() {
     });
 }
 
+objects[".main aside .show-lobby"].addEventListener("click", showLobby);
+
 function validateTimeInputValue() {
-    after = unixToDatetime(datetimeToUnix(objects[".working .time-input"].value));
-    if (after != objects[".working .time-input"].value) {
-        objects[".working .time-input"].value = unixToDatetime(Math.round((new Date()).getTime() / 1000)).replace(/\//g,"-");
+    after = unixToDatetime(datetimeToUnix(objects[".main > .working .time-input"].value));
+    if (after != objects[".main > .working .time-input"].value) {
+        objects[".main > .working .time-input"].value = unixToDatetime(Math.round((new Date()).getTime() / 1000)).replace(/\//g,"-");
     }
 }
 
 function loadTime() {
     edit = (typeof edit === "undefined") ? false : edit;
-    if (objects[".working .time-checkbox"].checked){
-        objects[".working .time-input"].disabled = true;
-        objects[".working .time-input"].value = unixToDatetime(Math.round((new Date()).getTime() / 1000));
+    if (objects[".main > .working .time-checkbox"].checked){
+        objects[".main > .working .time-input"].disabled = true;
+        objects[".main > .working .time-input"].value = unixToDatetime(Math.round((new Date()).getTime() / 1000));
     }else{
-        objects[".working .time-input"].disabled = false;
+        objects[".main > .working .time-input"].disabled = false;
     }
     setTimeout(loadTime, 1000);
 }
@@ -208,11 +270,11 @@ window.addEventListener("load", function () {
 });
 
 //To toggle sidebar
-objects[".header .toggle-sidebar"].addEventListener("click", function (e) {
+objects["header .toggle-sidebar"].addEventListener("click", function (e) {
     objects[".main aside"].classList.toggle("visible");
 });
 
-objects[".header .reload-page"].addEventListener("click", function (e) {
+objects["header .reload-page"].addEventListener("click", function (e) {
     if (!window.loading){
         loadLayout(function (callback) {
             setTimeout(callback, 1500);
@@ -220,8 +282,172 @@ objects[".header .reload-page"].addEventListener("click", function (e) {
     }
 });
 
-Array.prototype.forEach.call(_All(".main .aside .aside-item"), function (element) {
+Array.prototype.forEach.call(_All(".main aside .aside-item"), function (element) {
     element.addEventListener("click", function (e) {
         objects[".main aside"].classList.remove("visible");
     });
 });
+
+function syncHeight() {
+    editor = objects[".main > .working .editor-textarea"];
+    preview = objects[".main > .working .preview-div"];
+    percentage = editor.scrollTop / (editor.scrollHeight - editor.offsetHeight);
+    preview.scrollTop = (preview.scrollHeight - preview.offsetHeight) * percentage;
+}
+
+function previewText(event) {
+    objects[".main > .working .preview-div"].innerHTML = marked(objects[".main > .working .editor-textarea"].value);
+    syncHeight();
+}
+
+objects[".main > .working .editor-textarea"].addEventListener("change", previewText);
+objects[".main > .working .editor-textarea"].addEventListener("keypress", previewText);
+objects[".main > .working .editor-textarea"].addEventListener("keydown", previewText);
+objects[".main > .working .editor-textarea"].addEventListener("keyup", previewText);
+objects[".main > .working .editor-textarea"].addEventListener("blur", previewText);
+objects[".main > .working .editor-textarea"].addEventListener("scroll", syncHeight);
+
+function previewSlug() {
+    preview = objects[".main > .working .slug-preview"];
+    if (!objects[".main > .working .slug-input"].querySelector("paper-input-container").invalid && objects[".main > .working .slug-input"].value !== "") {
+
+        preview.innerHTML = window.location.host;
+
+        if (objects[".main > .working .type-radio-group"].selected == "writing") {
+            preview.innerHTML += "/writings/";
+        } else {
+            preview.innerHTML += "/pages/";
+        }
+
+        preview.innerHTML += objects[".main > .working .slug-input"].value;
+        preview.innerHTML += ".htm";
+
+        return;
+    }
+     preview.innerHTML = "";
+}
+
+function clearPreviousWorking() {
+    objects[".main > .working .title-input"].value = "";
+    objects[".main > .working .editor-textarea"].value = "";
+    objects[".main > .working .preview-div"].innerHTML = "";
+
+    objects[".main > .working .slug-input"].value = "";
+    objects[".main > .working .slug-preview"].innerHTML = "";
+
+    objects[".main > .working .type-radio-group"].selected = "writing";
+
+    objects[".main > .working .time-checkbox"].checked = true;
+
+    objects[".main > .working .id-input"].value = "";
+    objects[".main > .working .method-input"].value = "new";
+}
+
+function showWorking(edit, type, id) {
+    edit = (typeof edit === "undefined") ? false : edit;
+    type = (typeof type === "undefined") ? "writing" : type;
+    id = (typeof id === "undefined") ? "-1" : id;
+
+    loadLayout(function (callback) {
+
+        hideCurrentMainContainer();
+
+        uri = "/management/working/";
+        if (edit !== true) {
+            uri += "new";
+        } else {
+            uri += "edit?type=" + type + "&id=" + id;
+        }
+        pushState(uri);
+        clearPreviousWorking();
+
+        function finishShowing() {
+            objects[".main > .working"].classList.add("current");
+            callback();
+        }
+
+        if (edit === true) {
+            //Load Draft from Database
+        } else {
+            finishShowing();
+        }
+
+    });
+}
+
+objects[".main aside .show-working"].addEventListener("click", showWorking);
+
+objects[".main > .working .float-container .publish-fab"].addEventListener("click", function () {
+    objects[".main > .working .info-container"].classList.add("visible");
+});
+objects[".main > .working .info-container .close-button"].addEventListener("click", function () {
+    objects[".main > .working .info-container"].classList.remove("visible");
+});
+
+
+objects[".main > .working .type-radio-group"].addEventListener("click", previewSlug);
+
+objects[".main > .working .slug-input"].addEventListener("change", previewSlug);
+objects[".main > .working .slug-input"].addEventListener("keypress", previewSlug);
+objects[".main > .working .slug-input"].addEventListener("keydown", previewSlug);
+objects[".main > .working .slug-input"].addEventListener("keyup", previewSlug);
+objects[".main > .working .slug-input"].addEventListener("blur", previewSlug);
+
+objects[".main > .working .time-input"].addEventListener("blur", function () {
+    before = objects[".main > .working .time-input"].value;
+    after = unixToDatetime(datetimeToUnix(before));
+    if (after != objects[".main > .working .time-input"].value) {
+        objects[".main > .working .time-input"].value = unixToDatetime(Math.round((new Date()).getTime() / 1000)).replace(/\//g,"-");
+    }
+});
+
+objects[".main > .working .float-container .open-public-fab"].addEventListener("click", showPublic);
+
+function sendWorking(publish) {
+    queryString = new FormData();
+    queryString.append("_xsrf", getCookie("_xsrf"));
+    queryString.append("action", "save_working");
+
+
+    queryString.append("working_title", objects[".main > .working .title-input"].value);
+    queryString.append("working_content", objects[".main > .working .editor-textarea"].value);
+
+    queryString.append("working_type", objects[".main > .working .type-radio-group"].selected);
+    queryString.append("working_method", objects[".main > .working .method-input"].value);
+    queryString.append("working_time", datetimeToUnix(objects[".main > .working .time-input"].value));
+    if (publish) {
+        queryString.append("working_publish", "true");
+    } else {
+        queryString.append("working_publish", "false");
+    }
+    queryString.append("working_slug", objects[".main > .working .slug-input"].value);
+    queryString.append("working_id", objects[".main > .working .id-input"].value);
+
+    fetch("/management/api", {
+        "method": "post",
+        "credentials": "include",
+        "body": queryString
+    }).then(function (resp) {
+        if (response.status >= 200 && response.status < 400) {
+            return response;
+        }
+        throw response.status + "";
+    }).then(function (resp) {
+        return resp.json();
+    }).then(function (json) {
+        return;
+    }).catch(function (error) {
+        document.querySelector(".load-layout .loading-failed").show();
+    });
+}
+
+function buildWindow(slug, sub_slug) {
+    if (slug == "lobby") {
+        showLobby();
+    } else if (slug == "working") {
+        showWorking(sub_slug, getVars.type, getVars.id);
+    } else {
+        //need a more graceful way to deal with 404 Error.
+        window.location.href = "//" + window.location.host + "/404";
+    }
+}
