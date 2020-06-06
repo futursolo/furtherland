187b8897b1a3d998bb6e466bd1d6f4cff044d26e
Kaede Hoshikawa <futursolo@icloud.com>
2020-06-07 00:19:29 +0900

    Add option to restart.

--- frontend/signin/main.ts
+++ frontend/signin/main.ts
@@ -3,4 +3,4 @@ import FlSigninApp from "./fl-signin-app.riot";
 
 const mountApp = riot.component(FlSigninApp);
 
-const app = mountApp(document.querySelector("#app-root"), window.flAppOption);
+const app = mountApp(document.querySelector("#fl-app-root"), window.flAppOption);
