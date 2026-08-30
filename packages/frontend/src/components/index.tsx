292e636796c7b287e75330f82d416aa60d37d7b3
Kaede Hoshikawa <futursolo@icloud.com>
2026-08-30 10:22:11 +0000

    refactor: port header to astro

--- packages/frontend/src/components/index.tsx
+++ packages/frontend/src/components/index.tsx
@@ -3,6 +3,5 @@ export { default as Box } from './Box';
 export { default as ClientOnly } from './ClientOnly';
 export { default as H1 } from './H1';
 export { default as H2 } from './H2';
-export { default as Header } from './Header';
 export { default as Link } from './Link';
 export { default as Main, MainContainer } from './Main';
