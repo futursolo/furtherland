f58a8aab79baabf1d2fcc4ebb7b8d1cc6bce16eb
Kaede Hoshikawa <futursolo@icloud.com>
2026-08-30 10:53:33 +0000

    refactor: port main and main container to Astro

--- packages/frontend/src/components/index.tsx
+++ packages/frontend/src/components/index.tsx
@@ -5,4 +5,3 @@ export { default as H1 } from './H1';
 export { default as H2 } from './H2';
 export { default as Header } from './Header';
 export { default as Link } from './Link';
-export { default as Main, MainContainer } from './Main';
