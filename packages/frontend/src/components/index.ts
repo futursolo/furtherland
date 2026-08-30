11989893b349f5220c9c37badb442266acd5707f
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2026-08-30 21:22:33 +0900

    refactor: port h1, h2, links components to astro (#98)

--- packages/frontend/src/components/index.ts
+++ packages/frontend/src/components/index.ts
@@ -1,5 +1,2 @@
 export { default as Box } from './Box';
 export { default as ClientOnly } from './ClientOnly';
-export { default as H1 } from './H1';
-export { default as H2 } from './H2';
-export { default as Link } from './Link';
