aedb8890ee04be34ab617279ac9bbc58d666a301
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2026-08-30 21:29:37 +0900

    fix: use react component to render giscus (#99)

--- packages/frontend/src/components/PostComments.tsx
+++ packages/frontend/src/components/PostComments.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useRef } from 'react';
-
+import Giscus from '@giscus/react';
 import { useStore } from '@nanostores/react';
 
 import themeAtom from '@@frontend/atoms/theme';
@@ -16,50 +15,25 @@ const Comments = styled('section')({
   marginTop: '3rem',
 });
 
-const giscusTheme = {
-  light: 'light',
-  dark: 'dark',
-} as const;
-
 const GiscusComments = ({ slug }: PostCommentsProps) => {
-  const containerRef = useRef<HTMLDivElement>(null);
   const themeKind = useStore(themeAtom);
 
-  useEffect(() => {
-    const containerEl = containerRef.current;
-    const iframeEl = containerEl?.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
-    iframeEl?.contentWindow?.postMessage(
-      {
-        giscus: {
-          setConfig: {
-            theme: giscusTheme[themeKind],
-          },
-        },
-      },
-      'https://giscus.app',
-    );
-  }, [themeKind]);
-
   return (
-    <div ref={containerRef}>
-      <script
-        src="https://giscus.app/client.js"
-        async
-        crossOrigin="anonymous"
-        data-repo="futursolo/furtherland"
-        data-repo-id="MDEwOlJlcG9zaXRvcnkzMzExMDIzOQ=="
-        data-category="Post Comments"
-        data-category-id="DIC_kwDOAfk4384DEdL-"
-        data-mapping="specific"
-        data-term={`slug:${slug}`}
-        data-strict="1"
-        data-reactions-enabled="1"
-        data-emit-metadata="0"
-        data-input-position="top"
-        data-theme={giscusTheme[themeKind]}
-        data-lang="en"
-      />
-    </div>
+    <Giscus
+      id="comments"
+      repo="futursolo/furtherland"
+      repoId="MDEwOlJlcG9zaXRvcnkzMzExMDIzOQ=="
+      category="Post Comments"
+      categoryId="DIC_kwDOAfk4384DEdL-"
+      mapping="specific"
+      term={`slug:${slug}`}
+      strict="1"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme={themeKind}
+      lang="en"
+    />
   );
 };
 
