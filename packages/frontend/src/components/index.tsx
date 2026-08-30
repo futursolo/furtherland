c4be1dec3672ce292397c011e9e8bba7c9cf5804
Kaede Hoshikawa <futursolo@icloud.com>
2026-08-30 07:52:19 +0000

    refactor: port footer to astro

--- packages/frontend/src/components/index.tsx
+++ packages/frontend/src/components/index.tsx
@@ -1,7 +1,6 @@
 export { default as Author } from './Author';
 export { default as Box } from './Box';
 export { default as ClientOnly } from './ClientOnly';
-export { default as Footer } from './Footer';
 export { default as H1 } from './H1';
 export { default as H2 } from './H2';
 export { default as Header } from './Header';
