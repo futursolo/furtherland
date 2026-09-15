1e01b7099ba3510892a72b509381f65f46c18fd5
Kaede Hoshikawa <futursolo@icloud.com>
2026-09-15 13:40:00 +0000

    fix: migrate existing components

---
+++ packages/frontend-v5/src/components/react/Nav/index.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+
+import LinkItem from './LinkItem';
+import Styles from './Nav.module.scss';
+import ThemeToggle from './ThemeToggle';
+
+const Links = () => {
+  return (
+    <>
+      <a className={Styles['unstyled-link']} href="/" data-astro-reload>
+        <LinkItem colour="rgba(92, 184, 230, 0.9)">Home</LinkItem>
+      </a>
+      <a className={Styles['unstyled-link']} href="/pages/about" data-astro-reload>
+        <LinkItem colour="rgba(255, 242, 66, 0.9)">About</LinkItem>
+      </a>
+      <a className={Styles['unstyled-link']} href="/pages/links" data-astro-reload>
+        <LinkItem colour="rgba(230, 117, 92, 0.9)">Links</LinkItem>
+      </a>
+    </>
+  );
+};
+
+export const NavPlaceholder = () => <div className={Styles.placeholder} />;
+
+const Nav = () => {
+  const [layoutEl, setLayoutEl] = useState<HTMLElement | null>(null);
+  const [navPos, setNavPos] = useState<'top' | 'default'>('default');
+
+  useEffect(() => {
+    if (layoutEl) {
+      const listener = () => {
+        const { top } = layoutEl.getBoundingClientRect();
+
+        if (top <= 0) {
+          setNavPos('top');
+        } else {
+          setNavPos('default');
+        }
+      };
+
+      listener();
+
+      window.addEventListener('scroll', listener);
+      window.addEventListener('resize', listener);
+      window.screen.orientation.addEventListener('change', listener);
+      return () => {
+        window.screen.orientation.removeEventListener('change', listener);
+        window.removeEventListener('scroll', listener);
+        window.removeEventListener('resize', listener);
+      };
+    }
+
+    return () => {};
+  }, [layoutEl]);
+
+  return (
+    <nav className={Styles.layout} ref={setLayoutEl} style={{ paddingLeft: 0, paddingRight: 0 }}>
+      <nav
+        className={
+          navPos === 'top'
+            ? `${Styles.layout} ${Styles.inner} ${Styles['inner-top']}`
+            : `${Styles.layout} ${Styles.inner}`
+        }
+      >
+        <Links />
+        <div className={Styles['flex-space']} />
+        <ThemeToggle navPosition={navPos} />
+      </nav>
+    </nav>
+  );
+};
+
+export default Nav;
