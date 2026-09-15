ed0762b8bbf91606fc754fa0bac8d1031b8bc4d9
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2026-09-15 22:07:15 +0900

    refactor: remove tailwind (#130)

--- packages/frontend-v5/vite.config.ts
+++ packages/frontend-v5/vite.config.ts
@@ -4,11 +4,10 @@ import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tanstackStart(), viteReact()],
 })
 
 export default config
