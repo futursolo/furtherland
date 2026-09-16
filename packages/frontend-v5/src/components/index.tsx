991799f10bbd39a7042be555e0b0c4dbbc9731eb
Kaede Hoshikawa <futursolo@icloud.com>
2026-09-16 11:22:21 +0000

    feat: add lazy only component

--- packages/frontend-v5/src/components/index.tsx
+++ packages/frontend-v5/src/components/index.tsx
@@ -3,6 +3,7 @@ export { default as Box } from './Box';
 export { default as ClientOnly } from './ClientOnly';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
+export { default as LazyOnly } from './LazyOnly';
 export { default as Link } from './Link';
 export { default as Main, MainContainer } from './Main';
 export { default as Page } from './Page';
