4baca98b72f7285801edfe7766e472eed3a42e1e
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2026-09-17 21:39:28 +0900

    feat: enable view transition (#178)

--- packages/frontend-v5/src/router.tsx
+++ packages/frontend-v5/src/router.tsx
@@ -8,6 +8,7 @@ export function getRouter() {
     scrollRestoration: true,
     defaultPreload: 'intent',
     defaultPreloadStaleTime: 0,
+    defaultViewTransition: true,
   });
 
   return router;
