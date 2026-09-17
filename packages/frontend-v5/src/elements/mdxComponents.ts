6b7972a74cf6629eaaf4875dd820bdb3c83f3ef6
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2026-09-17 21:26:06 +0900

    fix: enable all mdx components (#176)

--- packages/frontend-v5/src/elements/mdxComponents.ts
+++ packages/frontend-v5/src/elements/mdxComponents.ts
@@ -3,6 +3,8 @@ import LazyOnly from '@@frontend-v5/components/LazyOnly';
 import Render from '@@frontend-v5/components/Render';
 
 import Anchor from './Anchor';
+import H1 from './H1';
+import H2 from './H2';
 import H3 from './H3';
 import Paragraph from './Paragraph';
 import Pre from './Pre';
@@ -17,6 +19,8 @@ import Table from './Table';
 // tables get a horizontal-scroll wrapper).
 export const mdxComponents = {
   a: Anchor,
+  h1: H1,
+  h2: H2,
   h3: H3,
   p: Paragraph,
   pre: Pre,
