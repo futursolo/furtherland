b5279e47880123c23bf52d4f855dd0aee5fe9cf3
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2026-09-17 18:10:20 +0900

    fix: apply some pending fixes (#159)

--- packages/frontend-v5/src/elements/index.tsx
+++ packages/frontend-v5/src/elements/index.tsx
@@ -2,6 +2,7 @@ export { default as Anchor } from './Anchor';
 export { default as H1 } from './H1';
 export { default as H2 } from './H2';
 export { default as H3 } from './H3';
+export { mdxComponents } from './mdxComponents';
 export { default as Paragraph } from './Paragraph';
 export { default as Pre } from './Pre';
 export { default as Table } from './Table';
