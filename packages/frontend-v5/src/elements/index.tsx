84cacf86427879bf50b7552fcb37bb67729d12ae
Kaede Hoshikawa <futursolo@icloud.com>
2026-09-18 14:07:23 +0000

    fix: add code background

--- packages/frontend-v5/src/elements/index.tsx
+++ packages/frontend-v5/src/elements/index.tsx
@@ -1,4 +1,5 @@
 export { default as Anchor } from './Anchor';
+export { default as Code } from './Code';
 export { default as H1 } from './H1';
 export { default as H2 } from './H2';
 export { default as H3 } from './H3';
