fb093a3f9e97b1cac7fe1fcb1cd0d04a6d708245
Kaede Hoshikawa <futursolo@icloud.com>
2026-09-19 04:16:52 +0000

    fix: cover safari glitch

--- packages/frontend-v5/src/components/ContentSkeleton.tsx
+++ packages/frontend-v5/src/components/ContentSkeleton.tsx
@@ -4,7 +4,7 @@ import Box from './Box';
 import Skeleton from './Skeleton';
 
 const Layout = styled(Box)({
-  paddingBottom: '2rem',
+  paddingBottom: '1rem',
 });
 
 const ContentSkeleton = () => (
--- packages/frontend-v5/src/components/Nav/index.tsx
+++ packages/frontend-v5/src/components/Nav/index.tsx
@@ -105,7 +105,7 @@ const Nav = () => {
       >
         <Links />
         <FlexSpace />
-        <ThemeToggle />
+        <ThemeToggle position={navPos} />
       </NavLayout>
     </NavLayout>
   );
