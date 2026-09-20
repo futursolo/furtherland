770aee6d58234720ee6e92a07748c17740de5446
Kaede Hoshikawa <futursolo@icloud.com>
2026-09-20 09:17:36 +0000

    fix: add code trim

--- packages/frontend/src/elements/Code.tsx
+++ packages/frontend/src/elements/Code.tsx
@@ -4,6 +4,7 @@ const Code = styled('code')(({ theme }) => ({
   backgroundColor: `${theme.colour.background.code.cssVar} !important`,
   padding: '0.2rem 0.4rem',
   borderRadius: 2,
+  lineHeight: '1.5rem',
 }));
 
 export default Code;
