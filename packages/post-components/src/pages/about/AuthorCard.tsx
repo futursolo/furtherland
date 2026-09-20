0fcc1fee480e7612a6b33b2bf118b5b475037732
Kaede Hoshikawa <futursolo@icloud.com>
2026-09-20 15:00:00 +0000

    feat: add author card layout

---
+++ packages/post-components/src/pages/about/AuthorCard.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const Layout = styled('div')({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+});
+
+const AuthorCard = () => {
+  return <Layout></Layout>;
+};
+
+export default AuthorCard;
