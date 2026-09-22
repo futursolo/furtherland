e1b5cd20196d87d437bd44cbd8df1a951e6a7871
Kaede Hoshikawa <futursolo@users.noreply.github.com>
2026-09-22 19:51:06 +0900

    refactor: move theme to common package (#239)

--- packages/common/src/theme.d.ts
+++ packages/common/src/theme.d.ts
@@ -1,4 +1,4 @@
-import type { Theme as ActualTheme } from '@@common/providers/theme';
+import type { Theme as ActualTheme } from './providers/theme';
 
 declare module '@emotion/react' {
   export interface Theme extends ActualTheme {}
