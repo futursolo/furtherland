d822af3e4390362d3c338d6c92ff465096445b1c
Kaede Hoshikawa <futursolo@icloud.com>
2026-09-22 07:15:43 +0000

    refactor: move theme to common package

--- packages/frontend/src/theme.d.ts
+++ packages/frontend/src/theme.d.ts
@@ -1,4 +1,4 @@
-import type { Theme as ActualTheme } from '@@frontend/providers/theme';
+import type { Theme as ActualTheme } from '@@common/providers/theme';
 
 declare module '@emotion/react' {
   export interface Theme extends ActualTheme {}
--- packages/frontend/src/utils/styled.tsx
+++ packages/frontend/src/utils/styled.tsx
@@ -1,3 +1,1 @@
-import styled from '@emotion/styled';
-
-export default styled;
+export { default } from '@@common/utils/styled';
