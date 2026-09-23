7292384727256d9b85898a4b88ee2109415e5aca
Kaede Hoshikawa <futursolo@icloud.com>
2026-09-23 04:10:19 +0000

    fix: move theme provider to common

--- packages/frontend/src/providers/theme.tsx
+++ packages/frontend/src/providers/theme.tsx
@@ -1,33 +1,3 @@
-import { type PropsWithChildren, useEffect } from 'react';
-
-import { ThemeProvider as BaseProvider, Global } from '@emotion/react';
-import { useAtomValue, useSetAtom } from 'jotai';
-import { useMediaQuery } from 'usehooks-ts';
-
-import { globalStyles, theme } from '@@common/providers/theme';
-import themeAtom, { getThemeKind } from '@@frontend/atoms/theme';
-
-const ThemeProvider = (props: PropsWithChildren) => {
-  const { children } = props;
-  const prefersDarkTheme = useMediaQuery('(prefers-color-scheme: dark)');
-  const themeKind = useAtomValue(themeAtom);
-  const setThemeKind = useSetAtom(themeAtom);
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies: getThemeKind is a stable function, no need to include it in deps
-  useEffect(() => {
-    setThemeKind(getThemeKind());
-  }, [prefersDarkTheme, setThemeKind]);
-
-  useEffect(() => {
-    document.documentElement.setAttribute('data-theme', themeKind);
-  }, [themeKind]);
-
-  return (
-    <BaseProvider theme={theme}>
-      <Global styles={globalStyles} />
-      {children}
-    </BaseProvider>
-  );
-};
+import ThemeProvider from '@@common/providers/theme';
 
 export default ThemeProvider;
